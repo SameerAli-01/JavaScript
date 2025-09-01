2fad45b93d683df9731c8482b764a679a016ce09
sameer <sameerali01s01@gmail.com>
2025-09-01 12:04:12 +0530

    function ass. js second day

--- JS-function/script.js
+++ JS-function/script.js
@@ -17,24 +17,43 @@ function sum(){
     let c = a + b;
     return c;
 }
-console.log(sum())
+console.log(sum());
 
 // 4.  Write a function that returns the  product of two numbers.
+function productNumber(val1, val2){
+   return val1 * val2;
+}
+console.log(productNumber(10, 10));
 
 // 5.  Write a function to  find the square  of a number. 
+function findSquare(a){
+    let square = a * a
+    return square;
+}
+console.log(findSquare(2));
 
 // 6.  Write a function to  find the cube  of a number. 
 
 // 7.  Write a function that converts  Celsius to Fahrenheit.
+function celsiusFahrenheit(celsius){
+    let c = (celsius * 9/5) + 32; 
+    return c;
+}
+console.log(celsiusFahrenheit(45));
 
 // 8.  Write a function that converts  Fahrenheit to Celsius.
-function fahrenheit(f){
-    let c = (f - 32) * 5/9
-    console.log(c);
+function fahrenheit(f2){
+    let m = (f2 - 32) * 5/9
+    return m;
 }
-fahrenheit(104)
+console.log(fahrenheit(104));
 
 // 9.  Write a function to calculate the  area of a rectangle.
+function areaofRectangle(length , width){
+    let area = length * width;
+    return area;
+}
+console.log(areaofRectangle(4, 5));
 
 // 10.  Write a function to calculate the  perimeter of a square.
 
@@ -52,6 +71,17 @@ function checkevenOdd(a){
 checkevenOdd(2)
 
 // 12.  Write a function to check if a number is  positive, negative, or zero.
+function checkNum(num){
+    if(num > 0){
+        console.log("you enter positive number: ");
+    }
+    else if (num < 0) {
+        console.log("you enter negative number: ");
+    } else {
+        console.log("Number is zero")
+    }
+}
+console.log(checkNum(-3));
 
 // 13.  Write a function to return the  largest of two numbers. 
 
@@ -61,11 +91,40 @@ checkevenOdd(2)
 
 // 16.  Write a function to check if a number is a  multiple of 5. 
 
-// 17.  Write a function to check if a year is a  leap year. 
+// 17.  Write a function to check if a year is a  leap year.
+function leapYear(year){
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+} 
+console.log(leapYear(2023))
 
 // 18.  Write a function to check if a character is a  vowel or consonant.
 
 // 19.  Write a function to return the  grade of a student  based on marks (A, B, C, D, F).
+function studentGrade(student){
+    switch (student){
+        case "A": 
+        console.log("Garade A");
+        break;
+
+        case "B": 
+        console.log("Garade B");
+        break;
+
+        case "C": 
+        console.log("Garade C");
+        break;
+
+        case "D": 
+        console.log("Garade D");
+        break;
+
+        default:
+        console.log("grade F");
+
+    }
+    return student;
+}
+studentGrade("B")
 
 // 20.  Write a function to check if a given string is a  palindrome.
 
